b5da392820ed7cc61781a492766c6db43505cedb
PoldiChen <1213386222@qq.com>
2018-03-25 21:28:27 +0800

    add question22 to question100
    add question22 to question100

---
+++ src/main/java/question050/BitCalculate.java
@@ -0,0 +1,17 @@
+package question050;
+
+public abstract class BitCalculate {
+	
+	public static void main(String[] args) {
+	
+		byte b1 = -128; // 1000,0000
+		byte b2 = -128;
+		byte result = (byte) (b1+b2);
+		System.out.println(result); // 0  1,0000,0000溢出的一位去掉，即0000,0000
+		
+		int i1 = Integer.MAX_VALUE; // 0111,1111,1111,1111,1111,1111,1111,1111
+		int i2 = Integer.MAX_VALUE;
+		System.out.println(i1+i2); // -2   1111,1111,1111,1111,1111,1111,1111,1110 -2的二进制表示
+	}
+
+}
